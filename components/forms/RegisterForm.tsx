928e1a86aa3732e52be02d6e4268e56bf452ad56
Asirwad <asirwadsali@gmail.com>
2024-12-25 17:22:27 +0530

    identification and verification added

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -16,10 +16,11 @@ import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldTypes } from "./PatientForm"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
-import { Doctors, GenderOptions } from "@/constants"
+import { Doctors, GenderOptions, IdentificationTypes } from "@/constants"
 import { Label } from "../ui/label"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
+import FileUploader from "../FileUploader"
 
 const RegisterForm = ({ user }: { user: User}) => {
     const router = useRouter();
@@ -49,7 +50,7 @@ const RegisterForm = ({ user }: { user: User}) => {
         <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
             <section className="mb-12 space-y-4">
-                <h1 className="header">Welcome</h1>
+                <h1 className="header">Welcome👋</h1>
                 <p className="text-dark-700">Schedule your first appointment.</p>
             </section>
             <section className="mb-12 space-y-6">
@@ -235,7 +236,48 @@ const RegisterForm = ({ user }: { user: User}) => {
                     label="Past medical history (if any)"
                     placeholder="Had UTI"
                 />
-            </div>          
+            </div>   
+
+            <section className="mb-12 space-y-6">
+                <div className="mb-9 space-y-1">
+                    <h2 className="sub-header">Identification and Verification</h2>
+                </div>
+            </section>   
+
+            <CustomFormField
+                    fieldType={FormFieldTypes.SELECT}
+                    control={form.control}
+                    name="identificationType"
+                    label="Identification type"
+                    placeholder="Select an identification type"
+            >
+                {IdentificationTypes.map((type) => (
+                    <SelectItem key={type} value={type}>
+                        {type}
+                    </SelectItem>
+                ))}
+            </CustomFormField>   
+            <CustomFormField
+                    fieldType={FormFieldTypes.INPUT}
+                    control={form.control}
+                    name="identificationNumber"
+                    label="Identification Number"
+                    placeholder="eg: 123456789"
+            /> 
+            <CustomFormField
+                        fieldType={FormFieldTypes.SKELETON}
+                        control={form.control}
+                        name="identificationDocument"
+                        label="Scanned copy of identification document"
+                        renderSkeleton={(field) => (
+                            <FormControl>
+                                <FileUploader 
+                                    files={field.value}
+                                    onChange={field.onChange}
+                                />
+                            </FormControl>
+                        )}
+                />
 
             <SubmitButton isLoading={isLoading} className="bg-purple-700 w-full rounded-lg">Get Started</SubmitButton>
         </form>
